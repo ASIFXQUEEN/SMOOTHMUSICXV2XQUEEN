2fbdc544b02cc1faad964150d70684d8f8826ad6
pagacmyrtis <153854739+pagacmyrtis@users.noreply.github.com>
2023-12-31 16:23:48 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/frndzchattingclub"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
