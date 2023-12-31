78589a6ec6508a6431c91c89a21a4a9dba8ccdba
pagacmyrtis <153854739+pagacmyrtis@users.noreply.github.com>
2023-12-31 16:27:17 +0530

    Update queue.py

--- BrandrdXMusic/utils/inline/queue.py
+++ BrandrdXMusic/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/BRANDEDKING82"
+                text="⛦ ᴏᴡɴᴇʀ 🥀", url=f"https://t.me/Arnavsingh70"
             ),
             InlineKeyboardButton(
-                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="⛦ ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/frndzchattingclub"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
